1bf66f23cd113677ae0387fd46501bd810f124e7
Maidasani <hitesh_maidasani@intuit.com>
2015-01-30 11:31:16 -0800

    adding updated IntuitWearKit framework and license directory

--- IntuitWearKit.framework/Headers/IWAppConfiguration.h
+++ IntuitWearKit.framework/Headers/IWAppConfiguration.h
@@ -4,10 +4,26 @@
 //
 //  Created by Osmon, Cindy on 12/31/14.
 //
-//  Copyright (c) 1/2/15 Intuit Inc. All rights reserved. Unauthorized reproduction is a
-//  violation of applicable law. This material contains certain confidential and proprietary
-//  information and trade secrets of Intuit Inc.
+// Copyright (c) 2015 Intuit Inc.
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import <Foundation/Foundation.h>
 #import <IntuitWearKit/IWearNotificationContent.h>
 
@@ -34,9 +50,9 @@ typedef NS_ENUM(NSInteger, IWGlanceColor){
 };
 
 /*!
- *  External constant that should be used to indicate the App Group name
+ *  External global variable that should be used to indicate the App Group name
  */
-extern NSString *const IWAppConfigurationApplicationGroupsPrimary;
+extern NSString *IWAppConfigurationApplicationGroupsPrimary;
 
 /*!
  *  External constant representing the Key to storing and retrieving
